a9dd14f8e8cd8a3d8a20afa765c10f935da04f8e
Jesús Martínez <gsus.martinez97@gmail.com>
2026-01-12 15:14:06 +0100

    Delete src/components directory

--- src/components/shared/RecipeEditorModal.jsx
+++
@@ -1,3 +0,0 @@
-// This file has been refactored into smaller modules
-// Import the new modular component
-export { default } from './RecipeEditorModal/RecipeEditorModal';
--- src/components/shared/RecipeEditorModal/IngredientsSection.jsx
+++
@@ -1,2 +0,0 @@
-// This file is no longer needed and can be removed in the future.
-// For now, it's empty to avoid breaking imports.
